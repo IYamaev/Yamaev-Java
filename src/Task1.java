b629755d172cd62b0bcc187a214aa7f25752e60b
Irik Yamaev <ymaev_irik12@mail.ru>
2022-06-11 15:19:24 +0300

    Task1 added

---
+++ src/Task1.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class Task1 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int inputNumber = sc.nextInt();
+        if (inputNumber > 7){
+            System.out.println("Привет");
+        }
+    }
+}
